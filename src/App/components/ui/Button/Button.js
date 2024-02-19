227f244da768cb2df1b54b7bf3efb3b478923f0d
champix56 <desorbaix@free.fr>
2024-02-19 15:23:16 +0100

    declaration de composants et usage

---
+++ src/App/components/ui/Button/Button.js
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const Button = () => {
+  return (
+    <View>
+      <Text>Benjamin</Text>
+    </View>
+  );
+};
+
+export default Button;
