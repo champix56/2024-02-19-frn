7c53308aa54b1408b429f8a1da6a8107cf535fdd
champix56 <desorbaix@free.fr>
2024-02-19 16:05:33 +0100

    usage des props

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 import Button from './components/ui/Button/Button';
+
 function App() {
   return (
     <View>
       {/*comment*/}
       <Text>DEMAT Breizh</Text>
-      <Button />
-      <Button />
-      <Button />
+      <Button text="button1" />
+      <Button text="fdfvscd" />
+      <Button text="button3" />
       <Button />
     </View>
   );
--- src/App/components/ui/Button/Button.js
+++ src/App/components/ui/Button/Button.js
@@ -1,10 +1,11 @@
 import {View, Text} from 'react-native';
 import React from 'react';
 
-const Button = () => {
+const Button = props => {
+  console.log(props);
   return (
     <View>
-      <Text>Benjamin</Text>
+      <Text>{props.text}</Text>
     </View>
   );
 };
