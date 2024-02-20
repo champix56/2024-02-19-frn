3d98c0ebfdb39e7d8cfe37fdedc9102ed9336a47
champix56 <desorbaix@free.fr>
2024-02-20 02:49:27 +0100

    React fragment

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import {View, Text, ToastAndroid, Image, StyleSheet} from 'react-native';
+import CarForm from './components/functionnal/CarForm/CarForm';
 
 function App() {
   const [state, setState] = useState({
@@ -11,9 +12,15 @@ function App() {
     present: true,
   });
   return (
-    <View>
-      <Text>{JSON.stringify(state)}</Text>
-    </View>
+    // react fragment <> ....</> permet l'existance d'un balise fictive
+    // pour laisser bien qu'une seul balise principale dans la section XML
+    //ne generre pas de composant en sortie
+    <>
+      <View>
+        <Text>{JSON.stringify(state)}</Text>
+      </View>
+      <CarForm />
+    </>
   );
 }
 
