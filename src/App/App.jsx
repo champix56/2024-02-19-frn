a33704775cf330f94a7f2c1805d6bd5d87e0dcb4
champix56 <desorbaix@free.fr>
2024-02-20 14:26:36 +0100

    gestion conditionnel de rendu sans else

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -8,8 +8,7 @@ const initialState = {
   couleur: 'bleu',
   marque: 'renault',
   model: 'megane II',
-  photo:
-    'https://th.bing.com/th/id/R.249c24da93d2105b0dcec7540c73ce1b?rik=5PakK0TbFfTVJA&riu=http%3a%2f%2fwww.encyclautomobile.fr%2fweb%2f_encyclauto%2fcars%2f263%2fmegane_2_2_f1_b.jpg&ehk=KTbl96eayslV9INUj6ejTTghHZ7kVn6IIEc8M7rj1tw%3d&risl=&pid=ImgRaw&r=0',
+  photo: undefined, //'https://th.bing.com/th/id/R.249c24da93d2105b0dcec7540c73ce1b?rik=5PakK0TbFfTVJA&riu=http%3a%2f%2fwww.encyclautomobile.fr%2fweb%2f_encyclauto%2fcars%2f263%2fmegane_2_2_f1_b.jpg&ehk=KTbl96eayslV9INUj6ejTTghHZ7kVn6IIEc8M7rj1tw%3d&risl=&pid=ImgRaw&r=0',
   disponible: true,
   prix: 2000,
 };
@@ -18,7 +17,9 @@ function App() {
   return (
     <View>
       {/*comment*/}
-      <Text style={styles.buttonText}>voiture: {JSON.stringify(car)}</Text>
+      <Text style={styles.buttonText}>
+        voiture: {JSON.stringify({...car, photo: undefined})}
+      </Text>
       <CarViewer car={car} />
     </View>
   );
--- src/App/components/ui/CarViewer/CarViewer.js
+++ src/App/components/ui/CarViewer/CarViewer.js
@@ -2,33 +2,42 @@ import {Image, Text, View} from 'react-native';
 import React, {useEffect, useState} from 'react';
 import styles from './CarViewer.styles';
 import PropTypes from 'prop-types';
-const initialState = true;
+
 const CarViewer = props => {
-  const [state, setState] = useState(initialState);
-  useEffect(() => {
-    return () => {};
-  }, [state]);
   return (
-      <View style={styles.CarViewer} testID="CarViewer">
-        <View style={styles.leftContainer}>
-          <Text style={styles.big}>{props.car.marque}</Text>
-          <Text style={styles.big}>{props.car.model}</Text>
-          <Text style={styles.big}>{props.car.imat}</Text>
-          <Text>{'\n'}</Text>
-          <Text>{props.car.couleur}</Text>
-          <Text style={[styles.big, styles.price]}>{props.car.prix}€</Text>
-          <Text>{'\n'}</Text>
-          <Text>
-            Disponible : {props.car.disponible === true ? 'oui' : 'non'}
-          </Text>
-        </View>
-        <View style={styles.rightContainer}>
-          <Text>{props.car.id}</Text>
-          <Image source={{uri: props.car.photo}} style={styles.image} />
-        </View>
+    <View style={styles.CarViewer} testID="CarViewer">
+      <View style={styles.leftContainer}>
+        <Text style={styles.big}>{props.car.marque}</Text>
+        <Text style={styles.big}>{props.car.model}</Text>
+        <Text style={styles.big}>{props.car.imat}</Text>
+        <Text>{'\n'}</Text>
+        <Text>{props.car.couleur}</Text>
+        <Text style={[styles.big, styles.price]}>{props.car.prix}€</Text>
+        <Text>{'\n'}</Text>
+        <Text>
+          Disponible : {props.car.disponible === true ? 'oui' : 'non'}
+        </Text>
       </View>
+      <View style={styles.rightContainer}>
+        <Text>{props.car.id}</Text>
+        {props.car.photo !== undefined && (
+          <Image source={{uri: props.car.photo}} style={styles.image} />
+        )}
+      </View>
+    </View>
   );
 };
-CarViewer.propTypes = {};
+CarViewer.propTypes = {
+  car: PropTypes.shape({
+    id: PropTypes.number,
+    marque: PropTypes.string.isRequired,
+    model: PropTypes.string.isRequired,
+    imat: PropTypes.string.isRequired,
+    couleur: PropTypes.string.isRequired,
+    prix: PropTypes.number.isRequired,
+    disponible: PropTypes.bool.isRequired,
+    photo: PropTypes.string,
+  }).isRequired,
+};
 CarViewer.defaultProps = {};
 export default CarViewer;
