b197f1aeae8385958a472b8daaa9be97cff22de9
champix56 <desorbaix@free.fr>
2024-02-20 15:47:31 +0100

    react.fragment <></>

--- src/App/components/functionnal/CarEditor/CarEditor.js
+++ src/App/components/functionnal/CarEditor/CarEditor.js
@@ -7,68 +7,78 @@ import {
   TouchableHighlight,
   View,
 } from 'react-native';
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import styles from './CarEditor.styles';
 import PropTypes from 'prop-types';
+import Button from '../../ui/Button/Button';
 const CarEditor = props => {
   const [car, setCar] = useState(props.car);
   return (
-    <View style={styles.CarEditor} testID="CarEditor">
-      <View style={styles.leftContainer}>
-        <TextInput
-          style={styles.big}
-          value={car.marque}
-          onChangeText={newStrValue => {
-            setCar({...car, marque: newStrValue});
-          }}
-        />
-        <TextInput
-          style={styles.big}
-          value={car.model}
-          onChangeText={newStrValue => {
-            setCar({...car, model: newStrValue});
-          }}
-        />
-        <TextInput
-          style={styles.big}
-          value={car.imat}
-          onChangeText={newStrValue => {
-            setCar({...car, imat: newStrValue});
-          }}
-        />
-        <TextInput
-          value={car.couleur}
-          onChangeText={newStrValue => {
-            setCar({...car, couleur: newStrValue});
-          }}
-        />
-        <TextInput
-          style={[styles.big, styles.price]}
-          value={car.prix.toString()}
-          onChangeText={newStrValue => {
-            setCar({...car, prix: parseFloat(newStrValue)});
-          }}
-        />
-        <Text>
-          Disponible :
-          <Switch
-            value={car.disponible}
-            onValueChange={newBoolValue => {
-              setCar({...car, disponible: newBoolValue});
+    <>
+      <Button
+        onButtonClicked={() => {
+          ToastAndroid.show('validation de formulaire', 1000);
+        }}>
+        Valider
+      </Button>
+      <View style={styles.CarEditor} testID="CarEditor">
+        <View style={styles.leftContainer}>
+          <TextInput
+            style={styles.big}
+            value={car.marque}
+            onChangeText={newStrValue => {
+              setCar({...car, marque: newStrValue});
             }}
           />
-        </Text>
+          <TextInput
+            style={styles.big}
+            value={car.model}
+            onChangeText={newStrValue => {
+              setCar({...car, model: newStrValue});
+            }}
+          />
+          <TextInput
+            style={styles.big}
+            value={car.imat}
+            onChangeText={newStrValue => {
+              setCar({...car, imat: newStrValue});
+            }}
+          />
+          <TextInput
+            value={car.couleur}
+            onChangeText={newStrValue => {
+              setCar({...car, couleur: newStrValue});
+            }}
+          />
+          <TextInput
+            style={[styles.big, styles.price]}
+            value={car.prix.toString()}
+            onChangeText={newStrValue => {
+              setCar({...car, prix: parseFloat(newStrValue)});
+            }}
+            keyboardType="decimal-pad"
+          />
+          <Text>
+            Disponible :
+            <Switch
+              value={car.disponible}
+              onValueChange={newBoolValue => {
+                setCar({...car, disponible: newBoolValue});
+              }}
+            />
+          </Text>
+        </View>
+        <View style={styles.rightContainer}>
+          <Text>{car.id}</Text>
+          <TouchableHighlight
+            onPress={() => {
+              ToastAndroid.show('Change image ask', 1000);
+            }}>
+            <Image source={{uri: car.photo}} style={styles.image} />
+          </TouchableHighlight>
+        </View>
       </View>
-      <View style={styles.rightContainer}>
-        <Text>{car.id}</Text>
-        <TouchableHighlight
-          onPress={() => {
-            ToastAndroid.show('Change image ask', 1000);
-          }}>
-          <Image source={{uri: car.photo}} style={styles.image} />
-        </TouchableHighlight>
-      </View>
-    </View>
+    </>
   );
 };
 CarEditor.propTypes = {
