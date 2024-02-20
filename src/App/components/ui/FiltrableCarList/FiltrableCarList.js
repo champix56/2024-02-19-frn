85b0488e6063c77d7ada4a9b4c8a8bca0537f1eb
champix56 <desorbaix@free.fr>
2024-02-20 22:45:05 +0100

    add detail viewer on list element p ressed an reale on repress + fix style

--- src/App/components/ui/FiltrableCarList/FiltrableCarList.js
+++ src/App/components/ui/FiltrableCarList/FiltrableCarList.js
@@ -1,36 +1,56 @@
-import {ScrollView, Text, TextInput, View} from 'react-native';
+import {Dimensions, ScrollView, Text, TextInput, View} from 'react-native';
 import React, {useEffect, useState} from 'react';
 import styles from './FiltrableCarList.styles';
 import PropTypes from 'prop-types';
 import CarListViewer from '../CarListViewer/CarListViewer';
+import CarViewer from '../CarViewer/CarViewer';
 const initialState = '';
 const FiltrableCarList = props => {
   const [state, setState] = useState(initialState);
+  const [current, setCurrent] = useState(undefined);
   return (
     <View style={styles.FiltrableCarList} testID="FiltrableCarList">
-      <Text style={{textAlign: 'center', fontSize: 20}}>
-        Liste des vehicules
-      </Text>
-      <TextInput
-        placeholder="rechercher..."
-        value={state}
-        onChangeText={str => setState(str)}
-        style={{
-          borderStyle: 'solid',
-          borderWidth: 1,
-          borderColor: 'grey',
-          margin: 5,
-        }}
-      />
-      <View style={{height: 400}}>
-        <ScrollView>
+      <View>
+        <Text style={styles.header}>Liste des vehicules</Text>
+        <TextInput
+          placeholder="rechercher..."
+          value={state}
+          onChangeText={str => setState(str)}
+          style={styles.input}
+        />
+      </View>
+      <View
+        style={[
+          styles.centerContainer,
+          {
+            height:
+              Dimensions.get('screen').height -
+              (current !== undefined ? 500 : 190),
+          },
+        ]}>
+        <ScrollView style={styles.scroll}>
           {props.cars
             .filter(e => e.model.toLowerCase().includes(state.toLowerCase()))
             .map((c, i) => (
-              <CarListViewer car={c} key={`c${i}`} onPress={() => {}} />
+              <CarListViewer
+                car={c}
+                key={`c${i}`}
+                onPress={() => {
+                  if (undefined === current || current !== c) {
+                    setCurrent(c);
+                  } else {
+                    setCurrent(undefined);
+                  }
+                }}
+              />
             ))}
         </ScrollView>
       </View>
+      {current !== undefined && (
+        <View style={{height: 100}}>
+          <CarViewer car={current} />
+        </View>
+      )}
     </View>
   );
 };
--- src/App/components/ui/FiltrableCarList/FiltrableCarList.styles.js
+++ src/App/components/ui/FiltrableCarList/FiltrableCarList.styles.js
@@ -1,6 +1,20 @@
 import {Dimensions, StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
-  FiltrableCarList: {height: Dimensions.get('screen').height - 20},
+  FiltrableCarList: {
+    height: Dimensions.get('window').height - 130,
+  },
+  header: {textAlign: 'center', fontSize: 20},
+  scroll: {
+    paddingTop: 2,
+    backgroundColor: '#D5E0E5',
+  },
+  input: {
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderColor: 'grey',
+    margin: 5,
+  },
+  centerContainer: {flexGrow: 1},
 });
 export default styles;
