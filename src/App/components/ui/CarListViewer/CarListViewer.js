5200eb3559165a12c16ad72ec74ad2bf2f149693
champix56 <desorbaix@free.fr>
2024-02-20 16:01:19 +0100

    CarListViewer

--- src/App/components/ui/CarListViewer/CarListViewer.js
+++ src/App/components/ui/CarListViewer/CarListViewer.js
@@ -1,17 +1,35 @@
-import {Text, View} from 'react-native';
+import {Image, Text, TouchableHighlight, View} from 'react-native';
 import React, {useEffect, useState} from 'react';
 import styles from './CarListViewer.styles';
 import PropTypes from 'prop-types';
 const initialState = true;
 const CarListViewer = props => {
-  const [state, setState] = useState(initialState);
-  useEffect(() => {
-    return () => {};
-  }, [state]);
   return (
-    <View style={styles.CarListViewer} testID="CarListViewer">
-      <Text>CarListViewer{JSON.stringify(props)}</Text>
-    </View>
+    <TouchableHighlight onPress={() => {}}>
+      <View style={styles.CarListViewer} testID="CarListViewer">
+        <View style={styles.left} testID="CarListViewer">
+          <Image
+            source={
+              props.car.photo !== undefined
+                ? {uri: props.car.photo}
+                : require('../../../../../assets/img/car.png')
+            }
+            style={styles.image}
+          />
+        </View>
+
+        <View style={styles.right} testID="CarListViewer">
+          <Text style={styles.titre}>
+            {props.car.id !== undefined
+              ? props.car.id + ':' + props.car.imat
+              : props.car.imat}
+          </Text>
+          <Text style={styles.titre}>
+            diponible :{props.car.disponible === true ? 'Oui' : 'Non'}
+          </Text>
+        </View>
+      </View>
+    </TouchableHighlight>
   );
 };
 CarListViewer.propTypes = {};
--- src/App/components/ui/CarListViewer/CarListViewer.styles.js
+++ src/App/components/ui/CarListViewer/CarListViewer.styles.js
@@ -1,6 +1,20 @@
 import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
-  CarListViewer: {},
+  CarListViewer: {
+    flexDirection: 'row',
+  },
+  left: {
+    flexGrow: 1,
+  },
+  image: {
+    width: 64,
+    height: 64,
+  },
+  titre: {
+    fontSize: 20,
+    fontWeight: '700',
+    color: 'grey',
+  },
 });
 export default styles;
