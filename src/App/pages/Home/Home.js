81502dce8ee169927597d5fba91eb9a6ea3f4924
champix56 <desorbaix@free.fr>
2024-02-20 23:10:14 +0100

    Home

---
+++ src/App/pages/Home/Home.js
@@ -0,0 +1,46 @@
+import {Image, Text, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import styles from './Home.styles';
+import PropTypes from 'prop-types';
+import Button from '../../components/ui/Button/Button';
+
+const initialState = true;
+const Home = props => {
+  return (
+    <View style={styles.Home} testID="Home">
+      <Text style={styles.titre}>Car lister</Text>
+      <View style={styles.centerImageView}>
+        <Image
+          source={require('../../../../assets/img/car.png')}
+          style={styles.centerImage}
+        />
+      </View>
+      <View style={styles.buttons}>
+        <Button onButtonClicked={() => {}} style={styles.button}>
+          <View style={styles.buttonView}>
+            <Image
+              source={require('../../../../assets/img/list.png')}
+              style={styles.buttonImage}
+            />
+            <Text>Nouveau</Text>
+          </View>
+        </Button>
+        <Button
+          onButtonClicked={() => {}}
+          bgColor="tomato"
+          style={styles.button}>
+          <View style={styles.buttonView}>
+            <Image
+              source={require('../../../../assets/img/plus.png')}
+              style={styles.buttonImage}
+            />
+            <Text>Nouveau</Text>
+          </View>
+        </Button>
+      </View>
+    </View>
+  );
+};
+Home.propTypes = {};
+Home.defaultProps = {};
+export default Home;
