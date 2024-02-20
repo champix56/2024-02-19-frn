0e2f0d9ce2d5ffbe3df3bf8ac732c052d74ddbe6
champix56 <desorbaix@free.fr>
2024-02-20 12:14:09 +0100

    avec image

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,13 +1,15 @@
 import React, {useEffect, useState} from 'react';
 import {View, Text} from 'react-native';
 import styles from './App.styles';
+import CarViewer from './components/ui/CarViewer/CarViewer';
 const initialState = {
   id: 0,
   imat: 'bs-709-gx',
   couleur: 'bleu',
   marque: 'renault',
   model: 'megane II',
-  photo: undefined,
+  photo:
+    'https://th.bing.com/th/id/R.249c24da93d2105b0dcec7540c73ce1b?rik=5PakK0TbFfTVJA&riu=http%3a%2f%2fwww.encyclautomobile.fr%2fweb%2f_encyclauto%2fcars%2f263%2fmegane_2_2_f1_b.jpg&ehk=KTbl96eayslV9INUj6ejTTghHZ7kVn6IIEc8M7rj1tw%3d&risl=&pid=ImgRaw&r=0',
   disponible: true,
   prix: 2000,
 };
@@ -17,6 +19,7 @@ function App() {
     <View>
       {/*comment*/}
       <Text style={styles.buttonText}>voiture: {JSON.stringify(car)}</Text>
+      <CarViewer car={car} />
     </View>
   );
 }
