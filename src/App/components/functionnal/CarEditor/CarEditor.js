4cbcd502009aaaf16a92dd45d5ed638e785a0cbb
champix56 <desorbaix@free.fr>
2024-02-20 14:02:25 +0100

    carEditor base

---
+++ src/App/components/functionnal/CarEditor/CarEditor.js
@@ -0,0 +1,47 @@
+import {
+  Image,
+  Switch,
+  Text,
+  TextInput,
+  ToastAndroid,
+  TouchableHighlight,
+  View,
+} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import styles from './CarEditor.styles';
+import PropTypes from 'prop-types';
+const initialState = true;
+const CarEditor = props => {
+  const [state, setState] = useState(initialState);
+  useEffect(() => {
+    return () => {};
+  }, [state]);
+  return (
+    <View style={styles.CarEditor} testID="CarEditor">
+      <View style={styles.leftContainer}>
+        <TextInput style={styles.big} value={props.car.marque} />
+        <TextInput style={styles.big} value={props.car.model} />
+        <TextInput style={styles.big} value={props.car.imat} />
+        <Text>{'\n'}</Text>
+        <TextInput value={props.car.couleur} />
+        <TextInput style={[styles.big, styles.price]} value={props.car.prix} />€
+        <Text>{'\n'}</Text>
+        <Text>
+          Disponible : <Switch value={props.car.disponible} />
+        </Text>
+      </View>
+      <View style={styles.rightContainer}>
+        <Text>{props.car.id}</Text>
+        <TouchableHighlight
+          onPress={() => {
+            ToastAndroid.show('Change image ask', 1000);
+          }}>
+          <Image source={{uri: props.car.photo}} style={styles.image} />
+        </TouchableHighlight>
+      </View>
+    </View>
+  );
+};
+CarEditor.propTypes = {};
+CarEditor.defaultProps = {};
+export default CarEditor;
