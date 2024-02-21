b812ba9d14e595c443375f9c1331ca3def195f4f
champix56 <desorbaix@free.fr>
2024-02-21 11:12:05 +0100

    connexion ancienne version sans react-redux

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,15 +1,36 @@
-import React, {useState} from 'react';
-import {View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {Text, View} from 'react-native';
 import styles from './App.styles';
 import {cars} from '../../db/db.json';
 import FiltrableCarList from './components/ui/FiltrableCarList/FiltrableCarList';
 import CarViewer from './components/ui/CarViewer/CarViewer';
+import {
+  ajouterValeur,
+  store,
+  initialState as storeInitialState,
+} from './store/store';
+import Button from './components/ui/Button/Button';
 const initialState = cars;
 function App() {
   const [cars, setCars] = useState(initialState);
   const [current, setCurrent] = useState(undefined);
+  const [state, setstate] = useState(store.getState());
+  console.log(store);
+  useEffect(() => {
+    store.subscribe(() => {
+      setstate(store.getState());
+    });
+  }, []);
   return (
     <View>
+
+      <Text>{JSON.stringify(state)}</Text>
+      <Button
+        onButtonClicked={() => {
+          store.dispatch(ajouterValeur(3));
+        }}>
+        Ajouter3
+      </Button>
       {current !== undefined && <CarViewer car={current} />}
 
       <FiltrableCarList
