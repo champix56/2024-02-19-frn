6ff2d84cde20fb1da33b432dd6085b828e61648a
champix56 <desorbaix@free.fr>
2024-02-21 11:34:52 +0100

    tranche avec ajout manuel de car et de array<car>

---
+++ src/App/store/carSlice.js
@@ -0,0 +1,36 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+const initialState = {
+  cars: [
+    {
+      id: 0,
+      imat: 'bs-709-gx',
+      couleur: 'bleu',
+      marque: 'renault',
+      model: 'megane II',
+      photo:
+        'https://th.bing.com/th/id/R.249c24da93d2105b0dcec7540c73ce1b?rik=5PakK0TbFfTVJA&riu=http%3a%2f%2fwww.encyclautomobile.fr%2fweb%2f_encyclauto%2fcars%2f263%2fmegane_2_2_f1_b.jpg&ehk=KTbl96eayslV9INUj6ejTTghHZ7kVn6IIEc8M7rj1tw%3d&risl=&pid=ImgRaw&r=0',
+      disponible: true,
+      prix: 2000,
+    },
+  ],
+};
+
+const carSlice = createSlice({
+  name: 'ressources',
+  initialState,
+  reducers: {
+    addCar(state, action) {
+      //return {...state, cars: [...state.cars, action.payload]};
+      state.cars.push(action.payload);
+    },
+    addCars(state, action) {
+      state.cars.splice(0);
+      state.cars.push(...action.payload);
+    },
+  },
+});
+
+export const {addCar, addCars} = carSlice.actions;
+const ressourcesReducer = carSlice.reducer;
+export default ressourcesReducer;
