db1a0e15150f16319d203886d7dc527031de1dcb
champix56 <desorbaix@free.fr>
2024-02-21 09:35:38 +0100

    mise en place de onclick sur list

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -3,12 +3,25 @@ import {View} from 'react-native';
 import styles from './App.styles';
 import {cars} from '../../db/db.json';
 import FiltrableCarList from './components/ui/FiltrableCarList/FiltrableCarList';
+import CarViewer from './components/ui/CarViewer/CarViewer';
 const initialState = cars;
 function App() {
   const [cars, setCars] = useState(initialState);
+  const [current, setCurrent] = useState(undefined);
   return (
     <View>
-      <FiltrableCarList cars={cars} />
+      <FiltrableCarList
+        cars={cars}
+        selectedCar={current}
+        onUpdateSelect={c => {
+          if (current !== undefined && current === c) {
+            setCurrent(undefined);
+          } else {
+            setCurrent(c);
+          }
+        }}
+      />
+      {/* <CarViewer c={current}/> */}
     </View>
   );
 }
